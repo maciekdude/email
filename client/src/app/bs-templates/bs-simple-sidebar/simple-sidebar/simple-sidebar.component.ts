a66c17f3bec8a2b843a64dc2006e94c068ae5499
jacksonvaughan <jacksonvaughan@us.ibm.com>
2017-05-23 15:42:47 -0400

    adding mock emails

--- client/src/app/bs-templates/bs-simple-sidebar/simple-sidebar/simple-sidebar.component.ts
+++ client/src/app/bs-templates/bs-simple-sidebar/simple-sidebar/simple-sidebar.component.ts
@@ -15,6 +15,30 @@ export class SimpleSidebarComponent implements OnInit {
 
   constructor(private authService: LoopbackLoginService) { }
 
+  emails = [
+    {
+      id:"1234",
+      from: "jackson@mail.com",
+      to: "process@insurance.com",
+      subject: "a nice little subject",
+      text:"  Please issue a certificate of insurance for: Lakewood Park Apartments, Attn: Liza Schumer 1001 Pine Wood Lane Durham, NC 27703 Please email the completed coi to Lakewood@realestateassoc.com, kaseylm4@gmail.com and cc: lbarraga@bbt.com.  Thank you."
+    },
+    {
+      id:"1245",
+      from: "jackson@mail.com",
+      to: "process@insurance.com",
+      subject: "please do this",
+      text:"  Please issue a certificate of insurance for: Lakewood Park Apartments, Attn: Liza Schumer 1001 Pine Wood Lane Durham, NC 27703 Please email the completed coi to Lakewood@realestateassoc.com, kaseylm4@gmail.com and cc: lbarraga@bbt.com.  Thank you."
+    },
+    {
+      id:"1256",
+      from: "jackson@mail.com",
+      to: "process@insurance.com",
+      subject: "can you add this person?",
+      text:"  Please issue a certificate of insurance for: Lakewood Park Apartments, Attn: Liza Schumer 1001 Pine Wood Lane Durham, NC 27703 Please email the completed coi to Lakewood@realestateassoc.com, kaseylm4@gmail.com and cc: lbarraga@bbt.com.  Thank you."
+    }
+  ]
+
   ngOnInit() {
   }
 
