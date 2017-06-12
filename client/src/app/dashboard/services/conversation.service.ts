198984f3029052e2bf294002793588badcca85c8
jacksonvaughan <jacksonvaughan@us.ibm.com>
2017-06-12 14:41:46 -0400

    added second conversation call

--- client/src/app/dashboard/services/conversation.service.ts
+++ client/src/app/dashboard/services/conversation.service.ts
@@ -4,6 +4,7 @@ import 'rxjs/Rx';
 import { Observable } from 'rxjs/Rx';
 
 import { LoopbackLoginService } from '../../auth/loopback/lb-login.service';
+import { CurrentDemoService } from './current-demo.service';
 
 @Injectable()
 export class ConversationService {
@@ -11,9 +12,21 @@ export class ConversationService {
   private access_token: string;
   private url: string;
 
-  constructor(private http: Http, private auth: LoopbackLoginService) {
+  currentDemo = this.currentDemoService.currentDemo
+
+  constructor(
+    private http: Http,
+    private auth: LoopbackLoginService,
+    private currentDemoService: CurrentDemoService
+  ) {
     this.access_token = auth.get().token
-    this.url = '/api/Conversation/message?access_token=' + this.access_token
+    this.url = '/api/Conversation' + this.currentDemo.id + '/message?access_token=' + this.access_token
+
+    // subscribe to demo change
+    this.currentDemoService.changeDemo.subscribe( (demo) =>{
+      this.currentDemo = demo
+      this.url = '/api/Conversation' + demo.id + '/message?access_token=' + this.access_token
+    })
   }
 
   sendMessage(message: string): Observable<any> {
