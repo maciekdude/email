e38f4b9ff5e5b82c8583e269bec40022238e54fe
jacksonvaughan <jacksonvaughan@us.ibm.com>
2017-07-10 10:38:36 -0400

    fixed bug where auto response would not update

--- client/src/app/dashboard/response/response.component.ts
+++ client/src/app/dashboard/response/response.component.ts
@@ -12,6 +12,7 @@ export class ResponseComponent implements OnInit {
 
   currentEmail: Email = null
   itemsMissing = null
+  autoReply:string
 
   constructor(
     private emailService: EmailService
@@ -21,26 +22,40 @@ export class ResponseComponent implements OnInit {
     this.emailService.emailChange.subscribe( (email) => {
       this.itemsMissing = []
       this.currentEmail = email
-      // this.responseHTML = this.currentEmail.response
+      this.autoReply = ''
+      // document.getElementById('responseHTML').innerHTML = this.currentEmail.response
       let entities = email.entities
       for(let i in entities){
         if(entities[i] == null){
           this.itemsMissing.push(i)
         }
       }
+      console.log(this.currentEmail)
+      if(!this.currentEmail.response){
+        this.buildAutoReply()
+      }
     })
   }
 
+  buildAutoReply(){
+    this.autoReply ="Hi, \n\nThank you for sending this through; however, we are missing a few items. Could you please provide us with the following: \n\n"
+    + this.itemsMissing.join('\n ') + "\n\n Thank you!"
+    console.log(this.autoReply)
+  }
+
   sendReply(){
+    this.currentEmail.response = this.autoReply
+    this.autoReply = ''
 
-    console.log(document.getElementById('responseHTML'))
-    console.log(document.getElementById('responseArea'))
-
-    this.currentEmail.response = document.getElementById('responseArea').innerHTML
-    console.log(this.currentEmail.response)
-    document.getElementById('responseHTML').innerHTML = this.currentEmail.response
-
-    document.getElementById('responseArea').innerHTML = ''
+    // console.log(document.getElementById('responseHTML'))
+    // console.log(document.getElementById('responseArea'))
+    //
+    // this.currentEmail.response = document.getElementById('responseArea').innerHTML
+    // console.log(this.currentEmail.response)
+    // // this.currentEmail.response
+    // // document.getElementById('responseHTML').innerHTML = this.currentEmail.response
+    //
+    // document.getElementById('responseArea').innerHTML = ''
   }
 
 }
